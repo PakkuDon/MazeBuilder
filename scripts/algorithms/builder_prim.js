4a92f27c1e27e8242a7699b37af588e3a6d61869
PakkuDon <pakku.pd@gmail.com>
2015-02-09 14:07:34 +1100

    Started work on Randomized Prim's algorithm.
    - Created PrimBuilder with fields and empty method bodies.

---
+++ scripts/algorithms/builder_prim.js
@@ -0,0 +1,11 @@
+function PrimBuilder() {
+    this.done = false;
+    this.edges = [];
+    this.set = new DisjointSet();
+
+    this.initialise = function(width, height) {
+    }
+
+    this.build = function(maze) {
+    }
+}
