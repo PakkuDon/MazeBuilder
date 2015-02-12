e7f1b4c26fa440d6875990d7b16458d242285736
PakkuDon <pakku.pd@gmail.com>
2015-02-12 23:48:45 +1100

    Completed more MazeGraphics methods
    - Implemented MazeGraphics#drawEdge().
    - Implemented MazeGraphics#drawMaze().
    - Implemented MazeGraphics#drawSolution().
    - Implemented MazeGraphics#clear().

--- scripts/maze_graphics.js
+++ scripts/maze_graphics.js
@@ -25,19 +25,44 @@ function MazeGraphics(canvasId) {
     /**
      * drawEdge() - Draws a line using the given edge's values.
      */
-    this.drawEdge = function(edge) {
+    this.drawEdge = function(edge, color) {
+        // Calculate coordinates of edge's end points
         var aX = edge.aX * this.cellWidth + this.xOffset;
         var aY = edge.aY * this.cellHeight + this.yOffset;
         var bX = edge.bX * this.cellWidth + this.xOffset;
         var bY = edge.bY * this.cellHeight + this.yOffset;
+
+        // Draw line
+        this.context.strokeStyle = color;
+        this.context.beginPath();
+        this.context.moveTo(aX, aY);
+        this.context.lineTo(bX, bY);
+        this.context.stroke();
     }
 
+    /**
+     * drawMaze() - Draws all maze edges.
+     */
     this.drawMaze = function(maze) {
+        for (var i = 0; i < maze.edges.length; i++) {
+            this.drawEdge(maze.edges[i], "#FFF");
+        }
     }
 
+    /**
+     * drawSolution() - Draw edges contained in edge list.
+     */
     this.drawSolution = function(edgeList) {
+        for (var i = 0; i < edgeList.length; i++) {
+            this.drawEdges(edgeList[i], "#369");
+        }
     }
 
+    /**
+     * clear() - Erases canvas contents.
+     */
     this.clear = function() {
+        this.context.fillStyle = "#000";
+        this.context.fillRect(0, 0, this.canvas.width, this.canvas.height);
     }
 }
