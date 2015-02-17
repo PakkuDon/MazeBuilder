6a218ce4b830224e4ae500666d656df062482dbd
PakkuDon <pakku.pd@gmail.com>
2015-02-17 23:02:42 +1100

    Added prototype for EllerBuilder with empty functions

---
+++ scripts/algorithms/builder_eller.js
@@ -0,0 +1,11 @@
+function EllerBuilder () {
+    this.done = false;
+    this.set = new DisjointSet();
+    this.nextRow = [];
+
+    this.initialise = function(width, height) {
+    }
+
+    this.build = function(maze) {
+    }
+}
