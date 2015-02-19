d4ae8acab96b5c2bea4447d292a34f67028aa6be
PakkuDon <pakku.pd@gmail.com>
2015-02-19 23:51:18 +1100

    Created PriorityQueue prototype with empty member functions
    - Added scripts/data_structures/priority_queue.js

---
+++ scripts/data_structures/priority_queue.js
@@ -0,0 +1,23 @@
+/**
+ * PriorityQueue
+ * Implemented as a binary heap.
+ */
+function PriorityQueue() {
+    this.queue = [];
+    this.size = 0;
+
+    this.add = function(data, priority) {
+    }
+
+    this.poll = function() {
+    }
+
+    this.heapUp = function() {
+    }
+
+    this.heapDown = function() {
+    }
+
+    this.swap = function(indexA, indexB) {
+    }
+}
