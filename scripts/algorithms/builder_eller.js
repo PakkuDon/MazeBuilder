d0dc684df4f0501429ab3438509a259ab51dfaca
PakkuDon <pakku.pd@gmail.com>
2015-03-03 21:50:41 +1100

    Wrote EllerBuilder#initialise() function.

--- scripts/algorithms/builder_eller.js
+++ scripts/algorithms/builder_eller.js
@@ -1,9 +1,26 @@
 function EllerBuilder () {
     this.done = false;
     this.set = new DisjointSet();
-    this.nextRow = [];
+    this.setList = [];
+    this.currentRow = 0;
+    this.currentCol = 0;
+    this.joinVertically = false;
 
     this.initialise = function(width, height) {
+        this.done = false;
+        this.set.clear();
+
+        // Empty set list
+        while (this.setList.length > 0) {
+            this.setList.pop();
+        }
+
+        // Reset location
+        this.currentRow = 0;
+        this.currentCol = 0;
+
+        // Set flag to create vertical connections
+        this.joinVertically = false;
     }
 
     this.build = function(maze) {
