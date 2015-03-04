1533b2ee88c73ffbc4ad0e85958d420f080ff299
PakkuDon <pakku.pd@gmail.com>
2015-03-04 23:47:33 +1100

    Wrote part of the code for the EllerBuilder#build() function
    - Wrote code to create adjacent cells.
    - Renamed a few fields for clarity. Or something. Maybe.

--- scripts/algorithms/builder_eller.js
+++ scripts/algorithms/builder_eller.js
@@ -2,9 +2,9 @@ function EllerBuilder () {
     this.done = false;
     this.set = new DisjointSet();
     this.setList = [];
-    this.currentRow = 0;
-    this.currentCol = 0;
-    this.joinVertically = false;
+    this.currentX = 0;
+    this.currentY = 0;
+    this.joinAdjacent = true;
 
     this.initialise = function(width, height) {
         this.done = false;
@@ -16,13 +16,63 @@ function EllerBuilder () {
         }
 
         // Reset location
-        this.currentRow = 0;
-        this.currentCol = 0;
+        this.currentX = 1;
+        this.currentY = 0;
 
-        // Set flag to create vertical connections
-        this.joinVertically = false;
+        // Set initial value for joinAdjacent flag
+        // Flag is used to determine whether we're on the
+        // first or second parse of a row
+        this.joinAdjacent = true;
     }
 
     this.build = function(maze) {
+        // If last row processed, set flag and return
+        if (this.currentY == maze.height - 1
+            && this.joinAdjacent == false) {
+            this.done = true;
+        }
+        else {
+            // Case: Connect adjacent cells
+            if (this.joinAdjacent === true) {
+                var cellA = maze.grid[this.currentX][this.currentY];
+                var cellB = maze.grid[this.currentX + 1][this.currentY];
+
+                // Decide if cells should be merged or not
+                if (Math.random() >= 0.5) {
+                    // TODO: See if merge needs to be checked here
+                    this.set.merge(cellA, cellB);
+                }
+
+                // Move position to next cell
+                this.currentX++;
+
+                // If end of row reached, create set list
+                // and move to next row
+                if (this.currentX == maze.width - 1) {
+                    // Empty set list
+                    while (this.setList.length > 0) {
+                        this.setList.pop();
+                    }
+
+                    // Add set roots to set list
+                    for (var i = 0; i < maze.width; i++) {
+                        var cell = maze[i][this.currentY];
+                        var setNode = this.set.getNode(cell);
+                        if (setNode.parent == setNode) {
+                            this.setList.push(cell);
+                        }
+                    }
+
+                    // Move to next row and reset x-position
+                    this.currentY++;
+                    this.currentX = 0;
+                }
+            }
+            // Case: Create vertical connections between sets
+            // Should be done for every row except for the last
+            else if (this.currentY < maze.width - 1) {
+
+            }
+        }
     }
 }
